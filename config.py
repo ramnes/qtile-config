f1ef186f17e6a67ddd3287e2ee2293c457a77f51
Guillaume Gelin <ramnes@1000mercis.com>
2018-11-14 15:05:02 +0100

    Replace scrot by gnome-screenshot

--- config.py
+++ config.py
@@ -116,7 +116,7 @@ def init_keys():
         Key([mod, "shift"], "r", lazy.restart()),
         Key([mod, "shift"], "q", lazy.shutdown()),
 
-        Key([], "Print", lazy.spawn("scrot")),
+        Key([], "Print", lazy.spawn("gnome-screenshot -i")),
         Key([], "Scroll_Lock", lazy.spawn(HOME + ".local/bin/i3lock -d")),
         Key([mod], "Delete", lazy.spawn("amixer set Master toggle")),
         Key([mod], "Prior", lazy.spawn("amixer set Master 5+")),
