6e9c64fd43804a016fb404c0872c197a7bbf8ff8
Guillaume Gelin <ramnes@numberly.com>
2020-07-29 17:27:32 +0200

    Move things around, refactor, and simplify

--- config.py
+++ config.py
@@ -26,89 +26,52 @@ ORANGE = "#dd6600"
 DARK_ORANGE = "#371900"
 
 
-def window_to_previous_column_or_group():
-    @lazy.function
-    def __inner(qtile):
-        layout = qtile.current_group.layout
-        group_index = qtile.groups.index(qtile.current_group)
-        previous_group_name = qtile.current_group.get_previous_group().name
+def window_to_previous_column_or_group(qtile):
+    layout = qtile.current_group.layout
+    group_index = qtile.groups.index(qtile.current_group)
+    previous_group_name = qtile.current_group.get_previous_group().name
 
-        if layout.name != "columns":
+    if layout.name != "columns":
+        qtile.current_window.togroup(previous_group_name)
+    elif layout.current == 0 and len(layout.cc) == 1:
+        if group_index != 0:
             qtile.current_window.togroup(previous_group_name)
-        elif layout.current == 0 and len(layout.cc) == 1:
-            if group_index != 0:
-                qtile.current_window.togroup(previous_group_name)
-        else:
-            layout.cmd_shuffle_left()
-    return __inner
+    else:
+        layout.cmd_shuffle_left()
 
 
-def window_to_next_column_or_group():
-    @lazy.function
-    def __inner(qtile):
-        layout = qtile.current_group.layout
-        group_index = qtile.groups.index(qtile.current_group)
-        next_group_name = qtile.current_group.get_next_group().name
+def window_to_next_column_or_group(qtile):
+    layout = qtile.current_group.layout
+    group_index = qtile.groups.index(qtile.current_group)
+    next_group_name = qtile.current_group.get_next_group().name
 
-        if layout.name != "columns":
+    if layout.name != "columns":
+        qtile.current_window.togroup(next_group_name)
+    elif layout.current + 1 == len(layout.columns) and len(layout.cc) == 1:
+        if group_index + 1 != len(qtile.groups):
             qtile.current_window.togroup(next_group_name)
-        elif layout.current + 1 == len(layout.columns) and len(layout.cc) == 1:
-            if group_index + 1 != len(qtile.groups):
-                qtile.current_window.togroup(next_group_name)
-        else:
-            layout.cmd_shuffle_right()
-    return __inner
+    else:
+        layout.cmd_shuffle_right()
 
 
-def window_to_previous_screen():
-    @lazy.function
-    def __inner(qtile):
-        i = qtile.screens.index(qtile.current_screen)
-        if i != 0:
-            group = qtile.screens[i - 1].group.name
-            qtile.current_window.togroup(group)
-    return __inner
+def window_to_previous_screen(qtile):
+    i = qtile.screens.index(qtile.current_screen)
+    if i != 0:
+        group = qtile.screens[i - 1].group.name
+        qtile.current_window.togroup(group)
 
 
-def window_to_next_screen():
-    @lazy.function
-    def __inner(qtile):
-        i = qtile.screens.index(qtile.current_screen)
-        if i + 1 != len(qtile.screens):
-            group = qtile.screens[i + 1].group.name
-            qtile.current_window.togroup(group)
-    return __inner
+def window_to_next_screen(qtile):
+    i = qtile.screens.index(qtile.current_screen)
+    if i + 1 != len(qtile.screens):
+        group = qtile.screens[i + 1].group.name
+        qtile.current_window.togroup(group)
 
 
-def switch_screens():
-    @lazy.function
-    def __inner(qtile):
-        i = qtile.screens.index(qtile.current_screen)
-        group = qtile.screens[i - 1].group
-        qtile.current_screen.set_group(group)
-    return __inner
-
-
-@hook.subscribe.client_new
-def set_floating(window):
-    normal_hints = window.window.get_wm_normal_hints()
-    if normal_hints and normal_hints["max_width"]:
-        window.floating = True
-        return
-
-    floating_classes = ("nm-connection-editor", "pavucontrol")
-    try:
-        if window.window.get_wm_class()[0] in floating_classes:
-            window.floating = True
-    except IndexError:
-        pass
-
-
-@hook.subscribe.screen_change
-def set_screens(event):
-    logger.debug("Handling event: {}".format(event))
-    subprocess.run(["autorandr", "--change"])
-    qtile.restart()
+def switch_screens(qtile):
+    i = qtile.screens.index(qtile.current_screen)
+    group = qtile.screens[i - 1].group
+    qtile.current_screen.set_group(group)
 
 
 def init_keys():
@@ -116,8 +79,8 @@ def init_keys():
         Key([mod], "Left", lazy.screen.prev_group(skip_managed=True)),
         Key([mod], "Right", lazy.screen.next_group(skip_managed=True)),
 
-        Key([mod, "shift"], "Left", window_to_previous_column_or_group()),
-        Key([mod, "shift"], "Right", window_to_next_column_or_group()),
+        Key([mod, "shift"], "Left", lazy.function(window_to_previous_column_or_group)),
+        Key([mod, "shift"], "Right", lazy.function(window_to_next_column_or_group)),
 
         Key([mod, "control"], "Up", lazy.layout.grow_up()),
         Key([mod, "control"], "Down", lazy.layout.grow_down()),
@@ -127,10 +90,10 @@ def init_keys():
         Key([mod, "mod1"], "Left", lazy.prev_screen()),
         Key([mod, "mod1"], "Right", lazy.next_screen()),
 
-        Key([mod, "shift", "mod1"], "Left", window_to_previous_screen()),
-        Key([mod, "shift", "mod1"], "Right", window_to_next_screen()),
+        Key([mod, "shift", "mod1"], "Left", lazy.function(window_to_previous_screen)),
+        Key([mod, "shift", "mod1"], "Right", lazy.function(window_to_next_screen)),
 
-        Key([mod], "t", switch_screens()),
+        Key([mod], "t", lazy.function(switch_screens)),
 
         Key([mod], "Up", lazy.group.prev_window()),
         Key([mod], "Down", lazy.group.next_window()),
@@ -210,6 +173,26 @@ def init_floating_layout():
     return layout.Floating(border_focus=ORANGE)
 
 
+def init_layouts():
+    margin = 0
+    if len(qtile.conn.pseudoscreens) > 1:
+        margin = 8
+    kwargs = dict(margin=margin, border_width=1, border_normal=DARK_GREY,
+                  border_focus=BLUE, border_focus_stack=ORANGE)
+    layouts = [
+        layout.Max(),
+        layout.Columns(num_columns=2, grow_amount=5, **kwargs)
+    ]
+
+    if DEBUG:
+        layouts += [
+            floating_layout, layout.Zoomy(), layout.Tile(), layout.Matrix(),
+            layout.TreeTab(), layout.MonadTall(margin=10), layout.RatioTile(),
+            layout.Stack()
+        ]
+    return layouts
+
+
 def init_widgets():
     widgets = [
         CurrentLayoutIcon(scale=0.6, padding=8),
@@ -249,28 +232,26 @@ def init_widgets():
     return widgets
 
 
-def init_top_bar():
-    main_bar = bar.Bar(widgets=init_widgets(), size=22, opacity=1)
-    # # only works with branch ramnes:multiple_gaps_per_position
-    # if DEBUG:
-    #     return [main_bar, bar.Bar(widgets=[], size=10)]
-    return main_bar
+@hook.subscribe.client_new
+def set_floating(window):
+    normal_hints = window.window.get_wm_normal_hints()
+    if normal_hints and normal_hints["max_width"]:
+        window.floating = True
+        return
+
+    floating_classes = ("nm-connection-editor", "pavucontrol")
+    try:
+        if window.window.get_wm_class()[0] in floating_classes:
+            window.floating = True
+    except IndexError:
+        pass
 
 
-def init_widgets_defaults():
-    return dict(font="DejaVu", fontsize=11, padding=2, background=DARK_GREY)
-
-
-def init_layouts():
-    margin = 0
-    if len(qtile.conn.pseudoscreens) > 1:
-        margin = 8
-    kwargs = dict(margin=margin, border_width=1, border_normal=DARK_GREY,
-                  border_focus=BLUE, border_focus_stack=ORANGE)
-    return [
-        layout.Max(),
-        layout.Columns(num_columns=2, grow_amount=5, **kwargs)
-    ]
+@hook.subscribe.screen_change
+def set_screens(event):
+    logger.debug("Handling event: {}".format(event))
+    subprocess.run(["autorandr", "--change"])
+    qtile.restart()
 
 
 @hook.subscribe.startup_complete
@@ -303,12 +284,8 @@ if __name__ in ["config", "__main__"]:
     groups = init_groups()
     floating_layout = init_floating_layout()
     layouts = init_layouts()
-    screens = [Screen(top=init_top_bar(), wallpaper="~/.background.jpg")]
-    widget_defaults = init_widgets_defaults()
-
-    if DEBUG:
-        layouts += [
-            floating_layout, layout.Zoomy(), layout.Tile(), layout.Matrix(),
-            layout.TreeTab(), layout.MonadTall(margin=10), layout.RatioTile(),
-            layout.Stack()
-        ]
+    widgets = init_widgets()
+    bar = bar.Bar(widgets=widgets, size=22, opacity=1)
+    screens = [Screen(top=bar, wallpaper="~/.background.jpg")]
+    widget_defaults = {"font": "DejaVu", "fontsize": 11, "padding": 2,
+                       "background": DARK_GREY}
