d2f55fbc82cf64aa4b6719799cd73a9c0458b1ab
ramnes <contact@ramnes.eu>
2023-10-23 18:19:33 +0200

    Blackify

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import os
 import psutil
 import socket
@@ -8,9 +7,21 @@ from libqtile import bar, hook, layout, qtile
 from libqtile.config import Click, Drag, Group, Key, Screen
 from libqtile.lazy import lazy
 from libqtile.log_utils import logger
-from libqtile.widget import (Battery, Clock, CurrentLayout, CurrentLayoutIcon,
-                             GroupBox, Notify, PulseVolume, Prompt, Sep,
-                             Spacer, Systray, TaskList, TextBox)
+from libqtile.widget import (
+    Battery,
+    Clock,
+    CurrentLayout,
+    CurrentLayoutIcon,
+    GroupBox,
+    Notify,
+    PulseVolume,
+    Prompt,
+    Sep,
+    Spacer,
+    Systray,
+    TaskList,
+    TextBox,
+)
 
 try:
     import aiomanhole
@@ -102,49 +113,37 @@ def init_keys():
     keys = [
         Key([mod], "Left", lazy.function(focus_previous_group)),
         Key([mod], "Right", lazy.function(focus_next_group)),
-
         Key([mod, "shift"], "Left", lazy.function(window_to_previous_column_or_group)),
         Key([mod, "shift"], "Right", lazy.function(window_to_next_column_or_group)),
-
         Key([mod, "control"], "Up", lazy.layout.grow_up()),
         Key([mod, "control"], "Down", lazy.layout.grow_down()),
         Key([mod, "control"], "Left", lazy.layout.grow_left()),
         Key([mod, "control"], "Right", lazy.layout.grow_right()),
-
         Key([mod, "mod1"], "Left", lazy.prev_screen()),
         Key([mod, "mod1"], "Right", lazy.next_screen()),
-
         Key([mod, "shift", "mod1"], "Left", lazy.function(window_to_previous_screen)),
         Key([mod, "shift", "mod1"], "Right", lazy.function(window_to_next_screen)),
-
         Key([mod], "t", lazy.function(switch_screens)),
-
         Key([mod], "Up", lazy.group.prev_window()),
         Key([mod], "Down", lazy.group.next_window()),
-
         Key([mod, "shift"], "Up", lazy.layout.shuffle_up()),
         Key([mod, "shift"], "Down", lazy.layout.shuffle_down()),
-
         Key([mod], "space", lazy.next_layout()),
         Key([mod, "shift"], "space", lazy.prev_layout()),
-
         Key([mod], "f", lazy.window.toggle_floating()),
         Key([mod, "shift"], "f", lazy.window.toggle_fullscreen()),
         Key([mod], "b", lazy.window.bring_to_front()),
         Key([mod], "s", lazy.layout.toggle_split()),
-
         Key([mod, "control"], "space", lazy.spawn("splatmoji type")),
         Key([mod], "g", lazy.labelgroup()),
         Key([mod], "r", lazy.spawn("rofi -show")),
         Key([mod], "u", lazy.spawn(browser)),
         Key([mod], "Return", lazy.spawn(terminal)),
         Key([mod], "BackSpace", lazy.window.kill()),
-
         Key([mod, "shift"], "r", lazy.reload_config()),
         Key([mod, "control"], "r", lazy.restart()),
         Key([mod, "shift"], "q", lazy.shutdown()),
         Key([mod], "v", lazy.validate_config()),
-
         Key([], "Print", lazy.spawn("gnome-screenshot -a")),
         Key([mod], "Print", lazy.spawn("gnome-screenshot -p")),
         Key([], "Scroll_Lock", lazy.spawn(screenlocker)),
@@ -154,7 +153,6 @@ def init_keys():
         Key([mod], "Insert", lazy.spawn("spotify-dbus playpause")),
         Key([mod], "End", lazy.spawn("spotify-dbus next")),
         Key([mod], "Home", lazy.spawn("spotify-dbus previous")),
-
         Key([mod], "F11", lazy.spawn("headset")),
         Key([mod], "F12", lazy.spawn("speakers")),
     ]
@@ -163,20 +161,36 @@ def init_keys():
             Key([mod], "Tab", lazy.layout.next()),
             Key([mod, "shift"], "Tab", lazy.layout.previous()),
             Key([mod, "shift"], "f", lazy.layout.flip()),
-            Key([mod, "shift"], "s", lazy.group["scratch"].dropdown_toggle("term"))
+            Key([mod, "shift"], "s", lazy.group["scratch"].dropdown_toggle("term")),
         ]
     return keys
 
 
 def init_mouse():
-    mouse = [Drag([mod], "Button1", lazy.window.set_position_floating(),
-                  start=lazy.window.get_position()),
-             Drag([mod], "Button3", lazy.window.set_size_floating(),
-                  start=lazy.window.get_size()),
-             Click([mod], "Button2", lazy.window.kill())]
+    mouse = [
+        Drag(
+            [mod],
+            "Button1",
+            lazy.window.set_position_floating(),
+            start=lazy.window.get_position(),
+        ),
+        Drag(
+            [mod],
+            "Button3",
+            lazy.window.set_size_floating(),
+            start=lazy.window.get_size(),
+        ),
+        Click([mod], "Button2", lazy.window.kill()),
+    ]
     if DEBUG:
-        mouse += [Drag([mod, "shift"], "Button1", lazy.window.set_position(),
-                       start=lazy.window.get_position())]
+        mouse += [
+            Drag(
+                [mod, "shift"],
+                "Button1",
+                lazy.window.set_position(),
+                start=lazy.window.get_position(),
+            )
+        ]
     return mouse
 
 
@@ -193,9 +207,19 @@ def init_groups():
 
     if DEBUG:
         from libqtile.config import DropDown, ScratchPad
-        dropdowns = [DropDown("term", terminal, x=0.125, y=0.25,
-                              width=0.75, height=0.5, opacity=0.8,
-                              on_focus_lost_hide=True)]
+
+        dropdowns = [
+            DropDown(
+                "term",
+                terminal,
+                x=0.125,
+                y=0.25,
+                width=0.75,
+                height=0.5,
+                opacity=0.8,
+                on_focus_lost_hide=True,
+            )
+        ]
         groups.append(ScratchPad("scratch", dropdowns))
     return groups
 
@@ -208,19 +232,29 @@ def init_layouts():
     margin = 0
     if len(qtile.core.conn.pseudoscreens) > 1:
         margin = 8
-    kwargs = dict(margin=margin, border_width=1, border_normal=GREY,
-                  border_focus=BLUE, border_focus_stack=ORANGE)
+    kwargs = dict(
+        margin=margin,
+        border_width=1,
+        border_normal=GREY,
+        border_focus=BLUE,
+        border_focus_stack=ORANGE,
+    )
     layouts = [
         layout.Max(),
-        layout.Columns(border_on_single=True, num_columns=2, grow_amount=5,
-                       **kwargs)
+        layout.Columns(border_on_single=True, num_columns=2, grow_amount=5, **kwargs),
     ]
 
     if DEBUG:
         layouts += [
-            floating_layout, layout.Bsp(fair=False), layout.Zoomy(), layout.Tile(),
-            layout.Matrix(), layout.TreeTab(), layout.MonadTall(margin=10),
-            layout.RatioTile(), layout.Stack()
+            floating_layout,
+            layout.Bsp(fair=False),
+            layout.Zoomy(),
+            layout.Tile(),
+            layout.Matrix(),
+            layout.TreeTab(),
+            layout.MonadTall(margin=10),
+            layout.RatioTile(),
+            layout.Stack(),
         ]
     return layouts
 
@@ -232,39 +266,65 @@ def parse_notification(message):
 def init_widgets():
     widgets = [
         CurrentLayoutIcon(scale=0.6, padding=8),
-        GroupBox(fontsize=9, padding=2, borderwidth=1, urgent_border=DARK_BLUE,
-                 disable_drag=True, highlight_method="border",
-                 this_screen_border=DARK_BLUE, other_screen_border=DARK_ORANGE,
-                 this_current_screen_border=BLUE,
-                 other_current_screen_border=ORANGE),
-
-        TextBox(text="◤", fontsize=45, padding=-1, foreground=DARK_GREY,
-                background=GREY),
-
-        TaskList(borderwidth=0, highlight_method="block", background=GREY,
-                 border=DARK_GREY, urgent_border=DARK_BLUE,
-                 markup_floating="<i>{}</i>", markup_minimized="<s>{}</s>"),
-
+        GroupBox(
+            fontsize=9,
+            padding=2,
+            borderwidth=1,
+            urgent_border=DARK_BLUE,
+            disable_drag=True,
+            highlight_method="border",
+            this_screen_border=DARK_BLUE,
+            other_screen_border=DARK_ORANGE,
+            this_current_screen_border=BLUE,
+            other_current_screen_border=ORANGE,
+        ),
+        TextBox(
+            text="◤", fontsize=45, padding=-1, foreground=DARK_GREY, background=GREY
+        ),
+        TaskList(
+            borderwidth=0,
+            highlight_method="block",
+            background=GREY,
+            border=DARK_GREY,
+            urgent_border=DARK_BLUE,
+            markup_floating="<i>{}</i>",
+            markup_minimized="<s>{}</s>",
+        ),
         Prompt(background=GREY),
         Systray(background=GREY),
-        TextBox(text="◤", fontsize=45, padding=-1,
-                foreground=GREY, background=DARK_GREY),
+        TextBox(
+            text="◤", fontsize=45, padding=-1, foreground=GREY, background=DARK_GREY
+        ),
         Notify(fmt=" 🔥 {} ", parse_text=parse_notification),
         PulseVolume(fmt=" {}", emoji=True, volume_app="pavucontrol"),
         PulseVolume(volume_app="pavucontrol"),
-        Clock(format=" ⏱ %H:%M  <span color='#666'>%A %d-%m-%Y</span>  ")
+        Clock(format=" ⏱ %H:%M  <span color='#666'>%A %d-%m-%Y</span>  "),
     ]
     if os.path.isdir("/sys/module/battery"):
-        widgets.insert(-1, Battery(format=" {char} {percent:2.0%} ",
-                                   charge_char="⚡", discharge_char="🔋",
-                                   full_char="⚡", unknown_char="⚡",
-                                   empty_char="⁉️ ", update_interval=2,
-                                   show_short_text=False,
-                                   default_text=""))
-        widgets.insert(-1, Battery(fmt="<span color='#666'>{}</span> ",
-                                   format="{hour:d}:{min:02d}",
-                                   update_interval=2, show_short_text=True,
-                                   default_text=""))
+        widgets.insert(
+            -1,
+            Battery(
+                format=" {char} {percent:2.0%} ",
+                charge_char="⚡",
+                discharge_char="🔋",
+                full_char="⚡",
+                unknown_char="⚡",
+                empty_char="⁉️ ",
+                update_interval=2,
+                show_short_text=False,
+                default_text="",
+            ),
+        )
+        widgets.insert(
+            -1,
+            Battery(
+                fmt="<span color='#666'>{}</span> ",
+                format="{hour:d}:{min:02d}",
+                update_interval=2,
+                show_short_text=True,
+                default_text="",
+            ),
+        )
     if DEBUG:
         widgets += [Sep(), CurrentLayout()]
     return widgets
@@ -286,9 +346,9 @@ def set_logging():
         qtile.debug()
 
 
-if aiomanhole:
-    @hook.subscribe.startup_complete
-    def set_manhole():
+@hook.subscribe.startup_complete
+def set_manhole():
+    if aiomanhole:
         aiomanhole.start_manhole(port=7113, namespace={"qtile": qtile})
 
 
@@ -314,5 +374,9 @@ if __name__ in ["config", "__main__"]:
     widgets = init_widgets()
     bar = bar.Bar(widgets=widgets, size=22, opacity=1)
     screens = [Screen(top=bar, wallpaper="~/.background.jpg")]
-    widget_defaults = {"font": "DejaVu", "fontsize": 11, "padding": 2,
-                       "background": DARK_GREY}
+    widget_defaults = {
+        "font": "DejaVu",
+        "fontsize": 11,
+        "padding": 2,
+        "background": DARK_GREY,
+    }
